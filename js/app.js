0483c3b81381a87c42f9f693411108eb8b4564f3
tomaswww <twingord@gmail.com>
2018-12-22 23:52:43 -0300

    fixed bug in shake function it was typo for length

--- js/app.js
+++ js/app.js
@@ -8,8 +8,7 @@ var max;
 function shakeAndBake(array){
   var pressBtn = document.getElementById("buttnSAB");
   pressBtn.addEventListener("click", function() {
-    var max = answers.lenght;
-    var indexAnswer = Math.round((Math.random() * ((max - 1) + 1)) + 1);
+    var indexAnswer = Math.round((Math.random() * ((answers.length - 1) + 1)) + 1);
     var setAnswer = document.getElementById("targetTextDisplay");
     setAnswer.innerHTML = answers[indexAnswer];
     // here the function to shake and randomly select answer
