e2eea2fa5574d58306905dba472aaaaacef988a9
tomaswww <twingord@gmail.com>
2018-12-23 10:24:08 -0300

    fixed bug in random nomber being set for choosing answer in appjs

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ var max;
 function shakeAndBake(array){
   var pressBtn = document.getElementById("buttnSAB");
   pressBtn.addEventListener("click", function() {
-    var indexAnswer = Math.round((Math.random() * ((answers.length - 1) + 1)) + 1);
+    var indexAnswer = Math.round(Math.random() * (answers.length - 1));
     var setAnswer = document.getElementById("targetTextDisplay");
     setAnswer.innerHTML = answers[indexAnswer];
     // here the function to shake and randomly select answer
