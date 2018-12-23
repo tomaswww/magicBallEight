dfff873ad1dd15128aa5bcf10271fe259e801301
tomaswww <twingord@gmail.com>
2018-12-22 23:49:22 -0300

    added answer function to display message, now must fix bug in max definition

--- js/app.js
+++ js/app.js
@@ -1,16 +1,21 @@
 //  object
-var answers = ["yes","no"]
-
+var answers = ["yes","no"];
+var max;
 
 
 
 // model
-function shakeAndBake(){
+function shakeAndBake(array){
   var pressBtn = document.getElementById("buttnSAB");
   pressBtn.addEventListener("click", function() {
+    var max = answers.lenght;
+    var indexAnswer = Math.round((Math.random() * ((max - 1) + 1)) + 1);
+    var setAnswer = document.getElementById("targetTextDisplay");
+    setAnswer.innerHTML = answers[indexAnswer];
     // here the function to shake and randomly select answer
   })
 };
 
 
 // view
+shakeAndBake(answers);
