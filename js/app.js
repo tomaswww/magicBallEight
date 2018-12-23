4f6bbc4636119ae83b2a70da4f9dc1162ef160f5
tomaswww <twingord@gmail.com>
2018-12-22 23:28:57 -0300

    added the shake button with some css for display and incomplete js to appjs

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
+//  object
+var answers = ["yes","no"]
 
+
+
+
+// model
+function shakeAndBake(){
+  var pressBtn = document.getElementById("buttnSAB");
+  pressBtn.addEventListener("click", function() {
+    // here the function to shake and randomly select answer
+  })
+};
+
+
+// view
