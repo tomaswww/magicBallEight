988a6904887fea8ee2c7e4faf3ac49da2dc73525
tomaswww <twingord@gmail.com>
2018-12-24 16:21:00 -0300

    added style to header and modify text in html and css

--- js/app.js
+++ js/app.js
@@ -1,30 +1,31 @@
 //  object
 var answers = ["It is certain.",
-"It is decidedly so.",
-"Without a doubt.",
-"Yes - definitely.",
-"You may rely on it.",
-"As I see it, yes.",
-"Most likely.",
-"Outlook good.",
-"Yes.",
-"Signs point to yes.",
-"Reply hazy, try again.",
-"Ask again later.",
-"Better not tell you now.",
-"Cannot predict now.",
-"Concentrate and ask again.",
-"Don't count on it.",
-"My reply is no.",
-"My sources say no.",
-"Outlook not so good.",
-"Very doubtful."];
+  "It is decidedly so.",
+  "Without a doubt.",
+  "Yes - definitely.",
+  "You may rely on it.",
+  "As I see it, yes.",
+  "Most likely.",
+  "Outlook good.",
+  "Yes.",
+  "Signs point to yes.",
+  "Reply hazy, try again.",
+  "Ask again later.",
+  "Better not tell you now.",
+  "Cannot predict now.",
+  "Concentrate and ask again.",
+  "Don't count on it.",
+  "My reply is no.",
+  "My sources say no.",
+  "Outlook not so good.",
+  "Very doubtful."
+];
 
 
 // model
-function shakeAndBake(array){
+function shakeAndBake(array) {
   var pressBtn = document.getElementById("buttnSAB");
-    pressBtn.addEventListener("click", function() {
+  pressBtn.addEventListener("click", function() {
     var indexAnswer = Math.round(Math.random() * (answers.length - 1));
     var setAnswer = document.getElementById("targetTextDisplay");
     shakeBall();
@@ -33,18 +34,16 @@ function shakeAndBake(array){
   })
 };
 
-function shakeBall(){
+function shakeBall() {
   var shakingIt = document.getElementById("theBall");
-  shakingIt.classList.add("animated","shake");
+  shakingIt.classList.add("animated", "wobble");
   var popingUp = document.getElementById("theBallText");
-  popingUp.classList.add("animated","fadeIn","delay-1s");
+  popingUp.classList.add("animated", "fadeIn", "delay-1s");
   setTimeout(function() {
-    var shakingIt = document.getElementById("theBall");
-    shakingIt.classList.remove("animated","shake");
-    var popingUp = document.getElementById("theBallText");
-    popingUp.classList.remove("animated","fadeIn","delay-1s");
-}, 1500);
-  }
+    shakingIt.classList.remove("animated", "wobble");
+    popingUp.classList.remove("animated", "fadeIn", "delay-1s");
+  }, 1500);
+}
 
 
 // view
